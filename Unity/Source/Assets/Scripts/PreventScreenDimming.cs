f8c783228ba73ff638a44ac3537224f349383723
Guilherme Novak <xr@guilhermenovak.com>
2021-04-25 14:21:56 -0300

    Prevent Screen Dimming
    - Implemented a script to prevent screen dimming.

---
+++ Unity/Source/Assets/Scripts/PreventScreenDimming.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventScreenDimming : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;        
+    }
+}
