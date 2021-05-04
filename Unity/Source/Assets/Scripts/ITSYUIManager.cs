590d524d13346378eafe0230ab1a689b7efa8780
Guilherme Novak <xr@guilhermenovak.com>
2021-05-04 14:47:41 -0300

    Working on Runtime Menu
    - Created all final buttons in the submenu.
    - Implemented Learn More button with URL.
    - Implemented Instructions menu.
    - Implemented screen overlay for the runtime menu.

--- Unity/Source/Assets/Scripts/ITSYUIManager.cs
+++ Unity/Source/Assets/Scripts/ITSYUIManager.cs
@@ -6,15 +6,40 @@ using UnityEngine.UI;
 public class ITSYUIManager : MonoBehaviour
 {
 
+    public GameObject menuButton;
     public GameObject subMenu;
+    public GameObject subMenuOverlay;
+    public GameObject instructions;
 
     public void Start()
     {
         subMenu.SetActive(false);
+        subMenuOverlay.SetActive(false);
+        instructions.SetActive(false);
     }
 
     public void ToggleSubMenu()
     {
         subMenu.SetActive(!subMenu.activeInHierarchy);
+        subMenuOverlay.SetActive(!subMenuOverlay.activeInHierarchy);
+    }
+
+    public void ShowInstructions()
+    {
+        menuButton.SetActive(false);
+        subMenuOverlay.SetActive(true);
+        instructions.SetActive(true);
+    }
+
+    public void HideInstructions()
+    {
+        menuButton.SetActive(true);
+        subMenuOverlay.SetActive(false);
+        instructions.SetActive(false);
+    }
+
+    public void LearnMore()
+    {
+        Application.OpenURL("https://www.bekindmovement.co.uk/");
     }
 }
